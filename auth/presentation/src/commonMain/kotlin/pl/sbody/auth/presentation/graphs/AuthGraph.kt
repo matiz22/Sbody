dcf297e4f84ab749a9ccb54024d6b4cea6b85826
matiz22 <mateuszmalich@gmail.com>
2024-08-06 14:31:54 +0200

    Auth Graph with welcome and login routes

---
+++ auth/presentation/src/commonMain/kotlin/pl/sbody/auth/presentation/graphs/AuthGraph.kt
@@ -0,0 +1,31 @@
+package pl.sbody.auth.presentation.graphs
+
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import androidx.navigation.navigation
+import pl.sbody.auth.presentation.routes.AuthRoutes
+import pl.sbody.auth.presentation.screens.LoginScreen
+import pl.sbody.auth.presentation.screens.WelcomeScreen
+import pl.sbody.auth.presentation.viewmodels.LoginViewModel
+import pl.sbody.core.presentation.di.util.koinViewModel
+
+fun NavGraphBuilder.authGraph(navController: NavController) {
+    navigation<AuthRoutes.AuthScreens>(startDestination = AuthRoutes.AuthScreens.WelcomeScreen) {
+        composable<AuthRoutes.AuthScreens.WelcomeScreen> {
+            WelcomeScreen(
+                onRegisterClick = { navController.navigate(AuthRoutes.AuthScreens.RegisterScreen) },
+                onLoginClick = { navController.navigate(AuthRoutes.AuthScreens.LoginScreen) },
+            )
+        }
+        composable<AuthRoutes.AuthScreens.LoginScreen> { navBackStackEntry ->
+            val loginViewModel =
+                koinViewModel<LoginViewModel>(viewModelStoreOwner = navBackStackEntry)
+            val loginScreenState by
+                loginViewModel.loginState.collectAsStateWithLifecycle(navBackStackEntry)
+            LoginScreen(loginScreenState = loginScreenState, onEvent = loginViewModel::onEvent)
+        }
+    }
+}
