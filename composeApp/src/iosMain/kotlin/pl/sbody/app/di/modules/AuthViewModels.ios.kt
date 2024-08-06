64a3abc69851571d873846bde299f10338744c11
matiz22 <mateuszmalich@gmail.com>
2024-08-06 14:44:19 +0200

    added register viewmodel to koin modules

--- composeApp/src/iosMain/kotlin/pl/sbody/app/di/modules/AuthViewModels.ios.kt
+++ composeApp/src/iosMain/kotlin/pl/sbody/app/di/modules/AuthViewModels.ios.kt
@@ -3,5 +3,9 @@ package pl.sbody.app.di.modules
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 import pl.sbody.auth.presentation.viewmodels.LoginViewModel
+import pl.sbody.auth.presentation.viewmodels.RegisterViewModel
 
-actual fun authViewModelsModule() = module { singleOf(::LoginViewModel) }
+actual fun authViewModelsModule() = module {
+    singleOf(::LoginViewModel)
+    singleOf(::RegisterViewModel)
+}
