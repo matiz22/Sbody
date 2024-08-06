38b56aab268902a0e6f072f2499359dfae902981
matiz22 <mateuszmalich@gmail.com>
2024-08-06 14:44:31 +0200

    added register route

--- auth/presentation/src/commonMain/kotlin/pl/sbody/auth/presentation/graphs/AuthGraph.kt
+++ auth/presentation/src/commonMain/kotlin/pl/sbody/auth/presentation/graphs/AuthGraph.kt
@@ -8,8 +8,10 @@ import androidx.navigation.compose.composable
 import androidx.navigation.navigation
 import pl.sbody.auth.presentation.routes.AuthRoutes
 import pl.sbody.auth.presentation.screens.LoginScreen
+import pl.sbody.auth.presentation.screens.RegisterScreen
 import pl.sbody.auth.presentation.screens.WelcomeScreen
 import pl.sbody.auth.presentation.viewmodels.LoginViewModel
+import pl.sbody.auth.presentation.viewmodels.RegisterViewModel
 import pl.sbody.core.presentation.di.util.koinViewModel
 
 fun NavGraphBuilder.authGraph(navController: NavController) {
@@ -26,6 +28,16 @@ fun NavGraphBuilder.authGraph(navController: NavController) {
             val loginScreenState by
                 loginViewModel.loginState.collectAsStateWithLifecycle(navBackStackEntry)
             LoginScreen(loginScreenState = loginScreenState, onEvent = loginViewModel::onEvent)
+        }
+        composable<AuthRoutes.AuthScreens.RegisterScreen> { navBackStackEntry ->
+            val registerViewModel =
+                koinViewModel<RegisterViewModel>(viewModelStoreOwner = navBackStackEntry)
+            val registerScreenState by
+                registerViewModel.registerState.collectAsStateWithLifecycle(navBackStackEntry)
+            RegisterScreen(
+                loginScreenState = registerScreenState,
+                onEvent = registerViewModel::onEvent,
+            )
         }
     }
 }
