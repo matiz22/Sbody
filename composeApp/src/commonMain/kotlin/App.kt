895ddcc51132d301f9fe41554c8ee0e16fbbffd4
matiz22 <mateuszmalich@gmail.com>
2024-08-06 14:31:27 +0200

    fixing boundaries of base surface

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -8,19 +8,34 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import org.koin.compose.KoinContext
+import pl.sbody.auth.presentation.graphs.authGraph
+import pl.sbody.auth.presentation.routes.AuthRoutes
 import pl.sbody.core.presentation.theme.AppTheme
 
 @Composable
 fun App() {
-    AppTheme(darkTheme = isSystemInDarkTheme()) {
-        Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colorScheme.background,
-        ) {
+    KoinContext {
+        AppTheme(darkTheme = isSystemInDarkTheme()) {
+            val navController = rememberNavController()
             Surface(
-                modifier = Modifier.fillMaxSize().windowInsetsPadding(WindowInsets.safeDrawing),
-                color = Color.Transparent,
-            ) {}
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colorScheme.background,
+            ) {
+                Surface(
+                    modifier = Modifier.fillMaxSize().windowInsetsPadding(WindowInsets.safeDrawing),
+                    color = Color.Transparent,
+                ) {
+                    NavHost(
+                        navController = navController,
+                        startDestination = AuthRoutes.AuthScreens,
+                    ) {
+                        authGraph(navController)
+                    }
+                }
+            }
         }
     }
 }
