6006bea783b5e0aee1be9047c1b19a5f7c1ff4d0
matiz22 <mateuszmalich@gmail.com>
2024-09-10 21:49:04 +0200

    calendar screen with widget

---
+++ calendar/presentation/src/commonMain/kotlin/pl/sbody/calendar/presentation/screens/CalendarScreen.kt
@@ -0,0 +1,52 @@
+package pl.sbody.calendar.presentation.screens
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import kotlinx.datetime.LocalDate
+import org.jetbrains.compose.resources.stringResource
+import pl.sbody.calendar.presentation.composables.CalendarWidget
+import pl.sbody.calendar.presentation.events.CalendarWidgetEvents
+import pl.sbody.calendar.presentation.models.UsersTrainingSlot
+import pl.sbody.calendar.presentation.states.CalendarWidgetState
+import sbody.calendar.presentation.generated.resources.Res
+import sbody.calendar.presentation.generated.resources.user_slots
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CalendarScreen(
+    calendarWidgetState: CalendarWidgetState,
+    pickedDaySlots: List<UsersTrainingSlot>,
+    onCalendarWidgetEvent: (CalendarWidgetEvents) -> Unit,
+    modifier: Modifier = Modifier.fillMaxSize(),
+    checkLocalDate: (LocalDate) -> Boolean,
+) {
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = stringResource(Res.string.user_slots))
+                },
+            )
+        },
+    ) { paddingValues ->
+        LazyColumn(
+            modifier = Modifier.fillMaxSize().padding(paddingValues),
+        ) {
+            item {
+                CalendarWidget(
+                    calendarWidgetState = calendarWidgetState,
+                    onCalendarWidgetEvent = onCalendarWidgetEvent,
+                    checkAvailability = checkLocalDate,
+                )
+            }
+        }
+    }
+}
