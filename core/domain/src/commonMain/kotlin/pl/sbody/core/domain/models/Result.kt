3f4c1089d6ab3fcf996673026f294c59137d50e9
matiz22 <mateuszmalich@gmail.com>
2024-09-10 22:00:28 +0200

    adding resutl state classes

---
+++ core/domain/src/commonMain/kotlin/pl/sbody/core/domain/models/Result.kt
@@ -0,0 +1,13 @@
+package pl.sbody.core.domain.models
+
+typealias RootError = Error
+
+sealed interface Result<out D, out E : RootError> {
+    data class Success<out D, out E : RootError>(
+        val data: D,
+    ) : Result<D, E>
+
+    data class Error<out D, out E : RootError>(
+        val error: E,
+    ) : Result<D, E>
+}
