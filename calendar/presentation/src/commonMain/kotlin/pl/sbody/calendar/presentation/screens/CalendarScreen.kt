ab0f1d8785879429b7acbe4f6fa2b4b2fd28ae64
matiz22 <mateuszmalich@gmail.com>
2024-09-12 10:39:22 +0200

    Slots inside screen with data as text

--- calendar/presentation/src/commonMain/kotlin/pl/sbody/calendar/presentation/screens/CalendarScreen.kt
+++ calendar/presentation/src/commonMain/kotlin/pl/sbody/calendar/presentation/screens/CalendarScreen.kt
@@ -1,19 +1,24 @@
 package pl.sbody.calendar.presentation.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import kotlinx.datetime.LocalDate
 import org.jetbrains.compose.resources.stringResource
 import pl.sbody.calendar.presentation.composables.CalendarWidget
+import pl.sbody.calendar.presentation.composables.SlotPosition
 import pl.sbody.calendar.presentation.events.CalendarWidgetEvents
 import pl.sbody.calendar.presentation.models.UsersTrainingSlot
 import pl.sbody.calendar.presentation.states.CalendarWidgetState
@@ -26,6 +31,7 @@ fun CalendarScreen(
     calendarWidgetState: CalendarWidgetState,
     pickedDaySlots: List<UsersTrainingSlot>,
     onCalendarWidgetEvent: (CalendarWidgetEvents) -> Unit,
+    navigateToDetails: (Int) -> Unit,
     modifier: Modifier = Modifier.fillMaxSize(),
     checkLocalDate: (LocalDate) -> Boolean,
 ) {
@@ -42,6 +48,8 @@ fun CalendarScreen(
         LazyColumn(
             modifier = Modifier.fillMaxSize().padding(paddingValues),
             contentPadding = PaddingValues(8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(8.dp),
         ) {
             item {
                 CalendarWidget(
@@ -49,6 +57,15 @@ fun CalendarScreen(
                     onCalendarWidgetEvent = onCalendarWidgetEvent,
                     checkAvailability = checkLocalDate,
                 )
+            }
+            item {
+                Text(
+                    text = calendarWidgetState.pickedDate.toString(),
+                    style = MaterialTheme.typography.headlineSmall,
+                )
+            }
+            items(pickedDaySlots) { slot ->
+                SlotPosition(slot = slot, navigateToDetails = { navigateToDetails(slot.id) })
             }
         }
     }
