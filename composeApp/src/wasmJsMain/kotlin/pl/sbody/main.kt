ddbca618b4df44d06b390ab943b09f85a28e843c
matiz22 <mateuszmalich@gmail.com>
2024-12-27 14:05:57 +0100

    Main files for web

---
+++ composeApp/src/wasmJsMain/kotlin/pl/sbody/main.kt
@@ -0,0 +1,15 @@
+@file:Suppress("ktlint:standard:filename")
+
+package pl.sbody
+
+import App
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.window.ComposeViewport
+import kotlinx.browser.document
+
+@OptIn(ExperimentalComposeUiApi::class)
+fun main() {
+    ComposeViewport(document.body!!) {
+        App()
+    }
+}
