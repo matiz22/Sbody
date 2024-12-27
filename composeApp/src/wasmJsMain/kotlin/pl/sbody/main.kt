220a8d9784c73efaa2b051b6be249070f976027e
matiz22 <mateuszmalich@gmail.com>
2024-12-27 14:14:41 +0100

    koin init

--- composeApp/src/wasmJsMain/kotlin/pl/sbody/main.kt
+++ composeApp/src/wasmJsMain/kotlin/pl/sbody/main.kt
@@ -6,9 +6,11 @@ import App
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
 import kotlinx.browser.document
+import pl.sbody.app.di.KoinInitializer
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
+    KoinInitializer().init()
     ComposeViewport(document.body!!) {
         App()
     }
